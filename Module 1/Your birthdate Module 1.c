14d148e41b18934dfc8d09a89ce44756b2079dc6
Sadariya <110012808+Sadariya@users.noreply.github.com>
2022-08-01 08:42:32 +0530

    Add files via upload

---
+++ Module 1/Your birthdate Module 1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	printf("my birthdate is 31st oct 1998");
+	return 0;
+}
---
+++ Module 1/Your name Module 1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	printf("my name is gaurav");
+	return 0;
+}
