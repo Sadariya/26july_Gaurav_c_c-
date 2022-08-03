847957086ed6b3a2eac77eb3608b1b742ec3aedb
Sadariya <110012808+Sadariya@users.noreply.github.com>
2022-08-03 08:43:47 +0530

    Delete Module 1 directory

--- Module 1/Your birthdate Module 1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("my birthdate is 31st oct 1998");
-	return 0;
-}
--- Module 1/Your name Module 1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("my name is gaurav");
-	return 0;
-}
