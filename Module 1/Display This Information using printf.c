efe010922e64f3a42953985437085073547888af
Sadariya <110012808+Sadariya@users.noreply.github.com>
2022-08-04 19:49:42 +0530

    Add files via upload

---
+++ Module 1/Display This Information using printf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("Name:-My name is Gaurav");
+	printf("\nBirthdate:-My birthdate is 31st oct 1998");
+	printf("\nAge:-My age is 23 Year old");
+	printf("\nAddress:-601,Sarswati palace,Panchasar road, Morbi");
+}
