b32861a9ad7386bf8c53babe2b20f74664d0fb1e
Sadariya <110012808+Sadariya@users.noreply.github.com>
2022-08-17 13:55:27 +0530

    Add files via upload

---
+++ Module 2/1.Write a program to make a square and cube of number.c.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int digit,square,cube;
+	printf("\nEnter Your squre and cube of number:");
+	scanf("%d",&digit);
+	square=digit*digit;
+	printf("\nYour square of number is:%d",square);
+	cube=digit*digit*digit;
+	printf("\nYour cube of number is:%d",cube);
+}
