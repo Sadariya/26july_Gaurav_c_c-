69de0c7069537bfe76303ea3f865c84f9dd223cb
Sadariya <110012808+Sadariya@users.noreply.github.com>
2022-09-03 09:19:34 +0530

    Update 3.Write a program user enter the 5 subjects mark. You have to make a total and find the percentage.c.c

--- module 3/3.Write a program user enter the 5 subjects mark. You have to make a total and find the percentage.c.c
+++ module 3/3.Write a program user enter the 5 subjects mark. You have to make a total and find the percentage.c.c
@@ -1,12 +1,12 @@
 /*
 Write a program user enter the 5 subjects mark.
 You have to make a total and find the percentage.
-a) percentage > 75 you have to print “Distinction”
-b) percentage > 60 and percentage <= 75 you have to print “First class”
-c) percentage >50 and percentage <= 60 you have to print “Second class”
-d) percentage > 35 and percentage <= 50 you have to print “Pass class”
-e) Otherwise print “Fail”
-Each subject marks is suppose that 100 marks so total marks is 600.
+a) percentage > 75 you have to print â€œDistinctionâ€
+b) percentage > 60 and percentage <= 75 you have to print â€œFirst classâ€
+c) percentage >50 and percentage <= 60 you have to print â€œSecond classâ€
+d) percentage > 35 and percentage <= 50 you have to print â€œPass classâ€
+e) Otherwise print â€œFailâ€
+Each subject marks is suppose that 100 marks so total marks is 500.
 */
 #include<stdio.h>
 void main()
@@ -26,7 +26,7 @@ void main()
 	printf("Enter english marks:");
 	scanf("%f",&e);
 	//Percentage :-
-	P=((a+b+c+d+e)*100)/600;
+	P=((a+b+c+d+e)*100)/500;
 	printf("Your percentage is:%f",P);
 	//Grade:-
     if (P>75)
