ae8c166d42f4d86aecc67173c4b8fcdeffbf5adf
Sadariya <110012808+Sadariya@users.noreply.github.com>
2022-09-20 17:53:32 +0530

    Update Task_8_file_management_table.c

--- Task/Task_8_file_management_table.c
+++ Task/Task_8_file_management_table.c
@@ -16,7 +16,7 @@ int main ()
 	p=fopen("C:/Users/sadar/OneDrive/Desktop/practise/Task_8_file_management_table.txt","r");
 	for (b=1; b<=10; b++)
 	{
-		fscanf(p,"%d%d%d",&A,&b);	
+		fscanf(p,"%d%d",&A,&b);	
 		printf("\n%d * %d = %d ",A,b,A*b);
 	}
 	fclose(p);
